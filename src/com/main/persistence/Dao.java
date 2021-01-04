5e557182b67b81fb09e10308ea8536062dedc16f
Stefano <stefano.pola.fargas@gmail.com>
2021-01-04 17:30:06 +0100

    Creacio classe Dao<T>

---
+++ src/com/main/persistence/Dao.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.main.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author stefano
+ *
+ */
+public interface Dao<T> {
+
+    Optional<T> get(long id);
+    
+    List<T> getAll();
+    
+    void save(T t);
+    
+    void update(T t, String[] params);
+    
+    void delete(T t);
+}
