6bf45479f3bcfb93fa88c9964c421c52bb839889
Stefano <stefano.pola.fargas@gmail.com>
2021-01-04 15:34:45 +0100

    Creacio classe ItemFactory

--- src/com/main/application/ItemFactory.java
+++ src/com/main/application/ItemFactory.java
@@ -2,6 +2,7 @@
  * 
  */
 package com.main.application;
+import com.main.domain.*;
 
 /**
  * @author stefano
@@ -9,4 +10,24 @@ package com.main.application;
  */
 public class ItemFactory {
 
+	public static Item createItem (String typeItem) {
+		
+		if (typeItem.toLowerCase().equals("flower")) {
+			
+			return new Flower();
+			
+		}else if (typeItem.toLowerCase().equals("tree")) {
+			return new Tree();
+			
+		}else if (typeItem.toLowerCase().equals("decoration")) {
+			
+			return new Decoration();
+		}else {
+			
+			System.out.println("This element is not possible to create. Choose a: flower,tree or decoration");
+			return null;
+		}
+		
+	}
+	
 }
