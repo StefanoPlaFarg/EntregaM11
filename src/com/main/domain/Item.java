2166416f29fe8e97bb18ca450c24d462cfe53a30
Stefano <stefano.pola.fargas@gmail.com>
2021-01-04 13:27:20 +0100

    Implementacio de la classe Item

--- src/com/main/domain/Item.java
+++ src/com/main/domain/Item.java
@@ -7,6 +7,38 @@ package com.main.domain;
  * @author stefano
  *
  */
-public interface Item {
+public abstract class Item {
+
+	private int id;
+	private double price = 0;
+	private static int COUNTER_ID = 1;
+
+	public Item() {
+
+		id = COUNTER_ID++;
+	}
+
+	public Item(double price) {
+		this.price = price;
+		id = COUNTER_ID++;
+	}
+
+	public int getId() {
+
+		return id;
+
+	}
+
+	public double getPrice() {
+
+		return price;
+
+	}
+
+	public void setPrice(double price) {
+
+		this.price = price;
+
+	}
 
 }
