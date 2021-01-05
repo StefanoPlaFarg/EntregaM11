4a36c0e0a558da87b409221115051ff83cce7cf9
Stefano <stefano.pola.fargas@gmail.com>
2021-01-05 16:02:32 +0100

    Creacio del metodes per crear Flowers, Trees i Decorations

--- src/com/main/application/ItemFactory.java
+++ src/com/main/application/ItemFactory.java
@@ -2,7 +2,11 @@
  * 
  */
 package com.main.application;
-import com.main.domain.*;
+
+import com.main.domain.Decoration;
+import com.main.domain.Flower;
+import com.main.domain.Item;
+import com.main.domain.Tree;
 
 /**
  * @author stefano
@@ -10,24 +14,23 @@ import com.main.domain.*;
  */
 public class ItemFactory {
 
-	public static Item createItem (String typeItem) {
-		
-		if (typeItem.toLowerCase().equals("flower")) {
-			
-			return new Flower();
-			
-		}else if (typeItem.toLowerCase().equals("tree")) {
-			return new Tree();
-			
-		}else if (typeItem.toLowerCase().equals("decoration")) {
-			
-			return new Decoration();
-		}else {
-			
-			System.out.println("This element is not possible to create. Choose a: flower,tree or decoration");
-			return null;
-		}
-		
+	public static Flower createFlower(double price, String colour) {
+
+		return new Flower(colour, price);
+
 	}
 	
+	public static Tree createFlower(double price, double height) {
+
+		return new Tree(height, price);
+
+	}
+	
+	 public static Decoration createDecoration(double price, String material) {
+
+		return new Decoration(material, price);
+
+	}
+	
+		
 }
