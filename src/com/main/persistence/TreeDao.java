aec40b9f6497fe80220c491d6f6fb559fb690336
Stefano <stefano.pola.fargas@gmail.com>
2021-01-05 11:36:10 +0100

    Creacio de tots els metodes TreeDao

---
+++ src/com/main/persistence/TreeDao.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.main.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.main.domain.Flower;
+import com.main.domain.Tree;
+
+/**
+ * @author stefano
+ *
+ */
+public class TreeDao implements Dao<Tree> {
+	
+	
+	private List<Tree> treesList = new ArrayList<>();
+
+	public TreeDao() {
+
+	}
+
+	@Override
+	public Tree get(int id) {
+
+		return treesList.get(id);
+	}
+
+	@Override
+	public void save(Tree tree) {
+		treesList.add(tree);
+
+	}
+
+	@Override
+	public void update(Tree flower, int id) {
+
+		if (treesList.get(id) != null) {
+
+			treesList.get(id).setPrice(flower.getPrice());
+			treesList.get(id).setHeight(flower.getHeight());
+
+		}
+
+	}
+
+	@Override
+	public void delete(int id) {
+		treesList.remove(id);
+	}
+
+	@Override
+	public List<Tree> getAll() {
+		return treesList;
+
+	}
+
+	@Override
+	public void removeAll() {
+		treesList.removeAll(treesList);
+	}
+
+}
